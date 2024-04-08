cfe07acfed29d20aed36f8ff62e79a701c619351
Martin Joukl <jouklma1@gmail.com>
2024-04-08 16:13:19 +0200

    Change RemoteToLocalLinkCoverter to service, fix tests

--- src/test/java/com/example/shipbrowser/integration/ShipIntegrationTest.java
+++ src/test/java/com/example/shipbrowser/integration/ShipIntegrationTest.java
@@ -1,21 +1,27 @@
 package com.example.shipbrowser.integration;
 
-import com.example.shipbrowser.controller.ShipController;
-import com.example.shipbrowser.repository.ShipRepository;
-import com.example.shipbrowser.repository.StoredImageRepository;
-import com.example.shipbrowser.service.ShipService;
-import com.example.shipbrowser.service.StoredImageService;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.mockserver.client.MockServerClient;
+import org.mockserver.integration.ClientAndServer;
+import org.mockserver.model.Header;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockserver.integration.ClientAndServer.startClientAndServer;
+import static org.mockserver.matchers.Times.exactly;
+import static org.mockserver.mock.OpenAPIExpectation.openAPIExpectation;
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
+import static org.mockserver.model.StringBody.exact;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 
@@ -25,14 +31,47 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 public class ShipIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
+
+    private ClientAndServer mockServer;
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    @BeforeEach
+    public void startServer() {
+        mockServer = startClientAndServer(65100);
+    }
+
+    @AfterEach
+    public void stopServer() {
+        mockServer.stop();
+    }
+
     @Test
     @WithMockUser(username = "user", password = "password")
-    void synchronizeShips() throws Exception {
-        mockMvc.perform(
-                put("/synchronizeShips")
-                        .accept(MediaType.APPLICATION_JSON)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .with(csrf())
-        );
+    void synchronizeShips() throws IOException {
+        byte[] shipData = resourceLoader.getResource("classpath:ships.json").getContentAsByteArray();
+        try (MockServerClient mockserverClient = new MockServerClient("localhost", 65100)) {
+            mockserverClient.when(
+                            request()
+                                    .withMethod("GET")
+                                    .withPath("/ships.json")
+                                    .withHeader("Accept", "application/json")
+                    )
+                    .respond(
+                            response()
+                                    .withStatusCode(200)
+                                    .withBody(shipData));
+
+
+            mockMvc.perform(
+                    put("/synchronizeShips")
+                            .accept(MediaType.APPLICATION_JSON)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .with(csrf())
+            );
+        } catch (Exception e) {
+            fail(e);
+        }
     }
 }
